a71be1705595e1dcf5aa2659a51830d05232a947
Jennifer <j_lopez48@u.pacific.edu>
2021-10-31 23:01:31 -0700

    Added description of class and additional case
    EXTRAHEALTH which would be another powerup for the player to use

--- src/PowerType.java
+++ src/PowerType.java
@@ -1,13 +1,16 @@
 public enum PowerType {
-	DOUBLESHOT, TRIPLESHOT, POWERSHOT, INCREASESPEED, NONE;        
+	DOUBLESHOT, TRIPLESHOT, POWERSHOT, INCREASESPEED, EXTRAHEALTH, NONE;        
+	
+	//This class will be used to determine if the player has collected a power up which will aid in their survival 
 	
 	public String toString() {
 		switch(this) {
 			case DOUBLESHOT: return "doubleshot";
 			case TRIPLESHOT: return "tripleshot";
 			case POWERSHOT: return "powershot";
-      			case INCREASESPEED: return "increasespeed";
-      			case NONE: return "none";
+      		case INCREASESPEED: return "increasespeed";
+      		case EXTRAHEALTH: return "extrahealth"; //added this case because I think it makes sense for one of the powertypes to be extra health
+      		case NONE: return "none";				//feel free to erase EXTRAHEALTH it if would be too hard to implement
 		}
 		return "n/a";
 	}
