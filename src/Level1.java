8f2e30bb4d4c856c21f458ed87bb8df477b71fe5
Ricardo <r_vazquezmendoza@u.pacific.edu>
2021-11-06 17:32:17 -0700

    Level 1

--- src/Level1.java
+++ src/Level1.java
@@ -1,4 +1,23 @@
 
 public class Level1 {
+	int playerX;
+	int playerY;
+	Boss boss;
+	boolean passed;
+	
+	void loadEnemies() {
+		
+		
+	}
+	
+	boolean isPassed() {
+		return passed;
+		
+	}
+	
+	void drawLevel() {
+		
+		
+	}
 	
 }
