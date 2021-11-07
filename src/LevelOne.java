914b689412c535544c8efd23f4f74b48db187f40
Ishaan Aggarwal <i_aggarwal1@u.pacific.edu>
2021-11-06 17:38:27 -0700

    changed level1 name to levelone

---
+++ src/LevelOne.java
@@ -0,0 +1,23 @@
+
+public class LevelOne {
+	int playerX;
+	int playerY;
+	Boss boss;
+	boolean passed;
+	
+	void loadEnemies() {
+		
+		
+	}
+	
+	boolean isPassed() {
+		return passed;
+		
+	}
+	
+	void drawLevel() {
+		
+		
+	}
+	
+}
