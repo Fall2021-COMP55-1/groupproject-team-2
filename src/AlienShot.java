1834f3f9c3ea31d99addd490fbd9462b1122de7e
Ishaan Aggarwal <i_aggarwal1@u.pacific.edu>
2021-11-13 17:10:41 -0800

    Rotated images for bullets

--- src/AlienShot.java
+++ src/AlienShot.java
@@ -1,12 +1,24 @@
 import acm.graphics.GImage;
 
 public class AlienShot extends Projectile {
-	public AlienShot(PowerType pt) {
-		super(pt);
+	//fields
+	private GImage ps;
+	private MainApplication program;
+	public AlienShot( PowerType pt, double d, double e, MainApplication ma) {
+		super(pt, d, e);
+		program = ma;
 		// TODO Auto-generated constructor stub
+		ps = new GImage("src/Bullets/Rotated Alien.png", d-48, e-48);
+		ps.setSize(64,64);
+		program.add(ps);
+		ps.sendToFront();
+
 	}
-	private GImage av;
-  public void draw() {
-  
-  }
+	
+	public PowerType shoot() {
+		for(int i = 0; i < 100; i++) {
+			ps.setLocation(xPos, yPos++);
+		}
+		return PowerType.POWERSHOT;
+	}
 }
