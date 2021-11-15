172fa2a885c4c87ae9ac5a892f83095e5f58e625
Ishaan Aggarwal <i_aggarwal1@u.pacific.edu>
2021-11-15 11:58:58 -0800

    Adding semi finished shooting mechanic.

--- src/AlienShot.java
+++ src/AlienShot.java
@@ -4,8 +4,8 @@ public class AlienShot extends Projectile {
 	//fields
 	private GImage ps;
 	private MainApplication program;
-	public AlienShot( PowerType pt, double d, double e, MainApplication ma) {
-		super(pt, d, e);
+	public AlienShot( double d, double e, MainApplication ma) {
+		super(d, e);
 		program = ma;
 		// TODO Auto-generated constructor stub
 		ps = new GImage("src/Bullets/Rotated Alien.png", d-48, e-48);
@@ -15,10 +15,9 @@ public class AlienShot extends Projectile {
 
 	}
 	
-	public PowerType shoot() {
+	public void shoot() {
 		for(int i = 0; i < 100; i++) {
 			ps.setLocation(xPos, yPos++);
 		}
-		return PowerType.POWERSHOT;
 	}
 }
