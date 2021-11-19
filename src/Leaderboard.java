f4ad906c9ba8cbae0b81f0e8216074e4dda04034
rvazque2 <rvazque2@STK-ECS-JR30343.stk.pacific.edu>
2021-11-19 11:53:43 -0800

    Leaderboard fix

--- src/Leaderboard.java
+++ src/Leaderboard.java
@@ -14,11 +14,8 @@ public class Leaderboard extends GraphicsPane {
 
     private MainApplication program; 
     private GImage background;
-    private GImage title;
-    private GImage button;
     private GImage bButton;
     private GLabel text;
-    private GLine textline;
     private String username = " ";
     private GImage leaderboard;
     private GImage leaderboardImage;
@@ -57,6 +54,7 @@ public class Leaderboard extends GraphicsPane {
 
     @Override
     public void mousePressed(MouseEvent e) {
+    	//boolean isOn = false;
         GObject obj = program.getElementAt(e.getX(), e.getY());
         if (obj == bButton) {
         	program.switchToMenu();
@@ -64,45 +62,14 @@ public class Leaderboard extends GraphicsPane {
         if (obj == leaderboard) {
         	program.switchToLeaderboard();
         }
-        /*if(obj == textbox) {
-
-            text.setFont("Courier");
-            text.sendToFront();
-            program.add(text);
-            textline = new GLine(800/2-150/2, 600*2/3-50, 800/2-150/2, 600*2/3-50+37);
-            textline.sendToFront();
-            program.add(textline);
-        }*/
+        
     }
 
     public void actionPerformed(ActionEvent e) {
-
+    	
     }
 
     public void keyPressed(KeyEvent e) {
-    	boolean isOn = false;
-        GObject obj = program.getElementAt(e.getX(), e.getY());
-        if (obj == bButton) {
-        	program.switchToMenu();
-        }
-        if (obj == leaderboard) {
-        	program.switchToLeaderboard();
-        }
     }
-    	/*if(e.getKeyChar() == KeyEvent.VK_ENTER && username != "") {
-    		//program.switchToGame();
-    		program.switchToLeaderboard();
-    	}
-        if(e.getKeyChar() != KeyEvent.VK_BACK_SPACE) {
-            username+= e.getKeyChar();
-            text.setLabel(username);
-        } else
-            if(username.length() > 1) {
-                username = username.substring(0,username.length()-2);
-                text.setLabel(username);
-            } else {
-                username = "";
-        		text.setLabel(username);
-            }
-    }*/
+    
 }
